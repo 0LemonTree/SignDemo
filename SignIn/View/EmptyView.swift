cbc337441df0fe75f5c5a693a4fdefebe8663993
Xu <abcxdx@163.com>
2022-06-19 19:01:55 +0800

    添加 emptyView 占位

---
+++ SignIn/View/EmptyView.swift
@@ -0,0 +1,20 @@
+//
+//  EmptyView.swift
+//  SignIn
+//
+//  Created by X on 2022/6/19.
+//
+
+import UIKit
+
+class EmptyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SignIn/ViewController/emptyView.swift
@@ -0,0 +1,20 @@
+//
+//  emptyView.swift
+//  SignIn
+//
+//  Created by X on 2022/6/19.
+//
+
+import UIKit
+
+class emptyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
